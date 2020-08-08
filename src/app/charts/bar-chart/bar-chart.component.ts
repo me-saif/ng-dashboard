e6f00f2e276d536dccc6ec933a00480f088e8a2f
Saif Shakil Shaikh <me.shaikhsaif@outlook.com>
2020-08-08 10:13:30 +0530

    Fixed Chart js

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 const SAMPLE_BARCHART_DATA: any[] = [
-  {data: [65, 59, 80, 81, 56, 54, 30], label: 'Q3 Sales'},
-  {data: [25, 39, 60, 91, 36, 54, 50], label: 'Q4 Sales'}
-]
+  { data: [65,59,80,81,56,54,30], label:'Q3 Sales'},
+  { data: [45,59,60,21,36,74,50], label:'Q4 Sales'}
+];
 
-const SAMPLE_BARCHART_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7']
+const  SAMPLE_BARCHART_LABELS: any[] = [
+ 'W1','W2','W3','W4','W5','W6','W7'
+]
 
 @Component({
   selector: 'app-bar-chart',
@@ -17,15 +19,15 @@ export class BarChartComponent implements OnInit {
   constructor() { }
 
   public barChartData: any[] = SAMPLE_BARCHART_DATA;
-  public barChartLabels: string[] = SAMPLE_BARCHART_LABELS;
-  public barChartType: 'bar';
-  public barChartLegend: false;
+  public barChartLabels: any[] = SAMPLE_BARCHART_LABELS;
+  public barChartType = 'bar';
+  public barChartLegends = false;
   public barChartOptions: any = {
-    scaleShowVerticallines: false,
-    reponsive: true
+    scaleShowVerticalLines: false,
+    responsive: true
   };
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
