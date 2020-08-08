5eb5e0baca9d160e59126693dc8480a7a836e1ea
Saif Shakil Shaikh <me.shaikhsaif@outlook.com>
2020-08-09 00:42:17 +0530

    Updated Server Component

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Server} from '../../shared/server';
+
+const SAMPLE_SERVERS = [
+  {id: 1, name: 'dev-web', isOnline: true},
+  {id: 2, name: 'dev-mail', isOnline: false},
+  {id: 3, name: 'prod-web', isOnline: true},
+  {id: 4, name: 'prod-mail', isOnline: true}
+]
 
 @Component({
   selector: 'app-section-health',
@@ -9,7 +17,9 @@ export class SectionHealthComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  servers: Server[] = SAMPLE_SERVERS;
+
+  ngOnInit(){
   }
 
 }
